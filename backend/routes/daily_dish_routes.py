b8ce4f466d6d37bcc41c353df8ff4d7628747352
ashley <ashleybarasebwa@gmail.com>
2025-03-24 17:28:14 -0400

    Updated tastebuds.html

--- backend/routes/daily_dish_routes.py
+++ backend/routes/daily_dish_routes.py
@@ -63,5 +63,40 @@ def restaurant_detail(id):
 
 @daily_dish_bp.route('/review', methods=['GET', 'POST'])
 def review():
-    user = {'firstName': 'Person-I-Know'} #Umm, don't know how to connect it with db right now
+    user = {'firstName': 'Person-I-Know'} #Umm, don't know how to connect it with db right now 
     return render_template('review.html', user = user)
+
+@daily_dish_bp.route('/TasteBuds')
+def get_matches():#This doesn't work correctly
+    try:
+        user_id = session.get('user_id')
+        if not user_id:
+            flash('Log in to view TasteBuddies.', 'error')
+            return redirect(url_for('auth.index'))
+
+
+        matches = db.session.query(
+            tasteComparisons.compare_to,
+            user.first_name,
+            user.last_name,
+            tasteComparisons.comparison_num
+        ).join(user, user.user_id == tasteComparisons.compare_to).filter(
+            tasteComparisons.compare_from == user_id,
+            tasteComparisons.comparison_num <= 6
+        ).order_by(tasteComparisons.comparison_num).all()
+
+
+
+
+        results =[{
+            "user_id": match.user_id,
+            "name": f"{match.first_name} {match.last_name}",
+            "match_score": round(( 24 - match.comparison_num) / 24 * 100, 1)
+        } for match in matches]
+
+
+        return render_template('TasteBuds.html', matches=results)
+
+
+    except Exception as e:
+        return jsonify({'status': 'error', 'message': str(e)}), 500
